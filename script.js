46e36d4f3e44114321ca90f3c99a712b6def3a26
Stanislav Minko <minkostas@gmail.com>
2023-02-12 12:27:11 +0300

    feat: task #2

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-let s = document.querySelector('.select');
-let w = document.querySelector('.wrapper');
+let s = document.querySelector(".select");
+let w = document.querySelector(".wrapper");
 
 /* s.style = 'border: 1px solid grey'; */
 
-s.addEventListener('click', function(){
-    if(w.hidden == true){
-        w.hidden = false;
-    } else{
-        w.hidden = true;
-    }
-})
+s.addEventListener("click", function () {
+  showWrapper();
+});
+
+w.addEventListener("click", function (event) {
+  s.textContent = event.target.textContent;
+  showWrapper();
+});
+
+function showWrapper() {
+  if (w.hidden == true) {
+    w.hidden = false;
+  } else {
+    w.hidden = true;
+  }
+}
